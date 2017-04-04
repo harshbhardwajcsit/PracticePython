fe6fc407ec5b6baaae64e495005fc84e06bf6065
harshbhardwaj29 <harshbhardwajcsit@gmail.com>
2017-04-05 00:30:38 +0530

    spoj codes

--- SPOJ/AddingRev/sol.py
+++ SPOJ/AddingRev/sol.py
@@ -1,14 +1,11 @@
-test=input()
-i=0
-while i!=test:
+test=int(input())
+for i in range(0,test):
    n1,n2=input().split()
 
 
    # reverse and remove trailing zero
    nn= str(n1.rstrip('0'))[::-1]
    np = str(n2.rstrip('0'))[::-1]
-   p=str(int(nn) + int(np))
+   p=str(int(nn) + int(np)).rstrip('0')
 
    print(p[::-1])
-
-i+=1
